ce7e49ede8bbbd56234b82240e231782cf965bf9
robfrank <ro.franchini@gmail.com>
2019-03-04 12:00:23 +0100

    #35 removes script

--- build_and_push_images.sh
+++
@@ -1,7 +0,0 @@
-./gradlew clean  bootRepackage buildDocker -x test -Pprod  -PnodeInstall --no-daemon
-
-docker push arcadeanalytics/arcadeanalytics:latest
-
-./gradlew clean  bootRepackage buildDockerSingle  -x test -Pprod  -PnodeInstall --no-daemon
-
-docker push arcadeanalytics/arcadeanalytics-single:latest
