1e0e9334b1210ff4f2e4ee9eb1a5df91e2d8cf43
arcade-player <arcade@arcadeanalytics.com>
2019-12-08 16:33:43 +0100

    #167
    sets default values for username and password, if non filled by the user, to n/a

--- src/main/java/com/arcadeanalytics/service/util/DataSourceUtil.java
+++ src/main/java/com/arcadeanalytics/service/util/DataSourceUtil.java
@@ -9,9 +9,9 @@ package com.arcadeanalytics.service.util;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,8 +36,8 @@ public class DataSourceUtil {
                 ds.getServer(),
                 ds.getPort(),
                 Optional.ofNullable(ds.getDatabase()).orElse("N/A"),
-                ds.getUsername(),
-                ds.getPassword(),
+                Optional.ofNullable(ds.getUsername()).orElse("N/A"),
+                Optional.ofNullable(ds.getPassword()).orElse("N/A"),
                 Optional.ofNullable(ds.isAggregationEnabled()).orElse(false),
                 Optional.ofNullable(ds.getConnectionProperties()).orElse("{}"),
                 Optional.ofNullable(ds.isRemote()).orElse(false),
